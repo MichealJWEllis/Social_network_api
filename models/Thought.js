3e1f8f505f84e0d0a069f865027318186987b0d5
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-06-07 16:33:49 -0400

    code edits

---
+++ models/Thought.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose')
+const dateFormat = require('../utils/dateFormat')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
